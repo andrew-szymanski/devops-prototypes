da1ed503d5d45e7cb6a45574c677d1ba0b315dc9
andrew-szymanski <madpole@donotreply.com>
2017-06-07 01:30:29 +0200

    mysql with curl and register script now working

--- poc-services/csd_clients/mysql/areg.sh
+++ poc-services/csd_clients/mysql/areg.sh
@@ -9,9 +9,9 @@ log() {
 # *** register service
 # ***************
 log "$0 starting..."
-
+curl -H "Accept: application/json" -H "Content-type: application/json"  -X POST --data @/tmp/registration.json https://swe1.celarius.com:1443/api/v1/groups
 
 log "$0 DONE"
 
-
+# {"code": "MYSQL","name": "MySQL","description": "mysql database","meta": ""}
 
