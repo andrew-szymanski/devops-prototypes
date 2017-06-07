e0b1652df589f073ebad25fb10b614e7b48ebd8e
andrew-szymanski <madpole@donotreply.com>
2017-06-07 21:26:21 +0200

    capture IP address for registration

--- poc-services/csd_clients/mysql/areg.sh
+++ poc-services/csd_clients/mysql/areg.sh
@@ -9,7 +9,26 @@ log() {
 # *** register service
 # ***************
 log "$0 starting..."
-curl -H "Accept: application/json" -H "Content-type: application/json"  -X POST --data @/tmp/registration.json https://swe1.celarius.com:1443/api/v1/groups
+
+# mickey mouse - write json to send
+JSON_FILE=/tmp/registration.json
+rm -fr ${JSON_FILE}
+echo '{' >> ${JSON_FILE}
+echo '"code": "MYSQL",' >> ${JSON_FILE}
+echo '"name": "MYSQL",' >> ${JSON_FILE}
+echo '"description": "mysql database",' >> ${JSON_FILE}
+# meta
+IP=$(hostname -i)
+printf '"meta": "%s"\n' "${IP}" >> ${JSON_FILE}
+echo '}' >> ${JSON_FILE} >> ${JSON_FILE}
+
+
+
+log "executing curl, json file:"
+log "------ start -------"
+cat ${JSON_FILE}
+log "------ end -------"
+curl -H "Accept: application/json" -H "Content-type: application/json"  -X POST --data @${JSON_FILE} https://swe1.celarius.com:1443/api/v1/groups
 
 log "$0 DONE"
 
