e876b3cd54a2323346791c8abe38fe049f730daa
andrew-szymanski <madpole@donotreply.com>
2017-06-08 22:03:56 +0200

    mysql server and client waiting for SDR now

--- poc-services/csd_clients/mysql-client/loop.sh
+++ poc-services/csd_clients/mysql-client/loop.sh
@@ -12,7 +12,8 @@ log() {
 }
  
 insert_row() {
-    mysql --host=${DATABASE_SERVER} --database=${DATABASE_NAME} --user=${USER_NAME} --password=${PASSWORD} -e "INSERT INTO mytable (sometext) VALUES (\"test\")"
+    TIMESTAMP=$(date "%H:%M:%S")
+    mysql --host=${DATABASE_SERVER} --database=${DATABASE_NAME} --user=${USER_NAME} --password=${PASSWORD} -e "INSERT INTO mytable (sometext) VALUES (\"$TIMESTAMP: $HOSTNAME $COUNTER\")"
 }
 
 
@@ -21,15 +22,20 @@ insert_row() {
 # ***************
 log "$0 starting..."
 
-
+# should be provided by service discovery
 DATABASE_NAME=${DATABASE_NAME:=mydb}
 USER_NAME=${USER_NAME=myuser}
 PASSWORD=${PASSWORD=mysql}
 DATABASE_SERVER=${DATABASE_SERVER=172.20.0.2}   # should not have default, but set for manual testing
 
+# this docker
+
+
 SLEEP_TIME=${SLEEP_TIME:=3}
+COUNTER=0
 while true; do
-    log "going to sleep for [${SLEEP_TIME}] secs"
+    ((COUNTER++))
+    log "${COUNTER}: going to sleep for [${SLEEP_TIME}] secs"
     sleep ${SLEEP_TIME}
 done
 
