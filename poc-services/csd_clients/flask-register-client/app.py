1abfa01aad9400337a491c708b15c81f7e6ac543
andrew-szymanski <madpole@donotreply.com>
2017-06-08 23:38:47 +0200

    flask now reads from mysql and displays

--- poc-services/csd_clients/flask-register-client/app.py
+++ poc-services/csd_clients/flask-register-client/app.py
@@ -1,9 +1,24 @@
 from flask import Flask
+from flask.ext.mysql import MySQL
+
 app = Flask(__name__)
+mysql = MySQL()
+ 
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'myuser'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'mysql'
+app.config['MYSQL_DATABASE_DB'] = 'mydb'
+app.config['MYSQL_DATABASE_HOST'] = '172.17.0.2'
+mysql.init_app(app)
+
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized'
+    conn = mysql.connect()
+    cur = conn.cursor()
+    cur.execute('''SELECT * FROM mytable''')
+    rv = cur.fetchall()
+    return str(rv)
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
